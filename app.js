dd0a5591745f99ca327dc9da437d81120d09d85e
Tran Vu Binh <tranb4010@gmail.com>
2024-03-13 19:38:06 +0700

    setup project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors');
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.json({message: "Welcome to my bookstore."});
+});
+
+module.exports = app;
+

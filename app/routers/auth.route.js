579409b9859fbbf03636ef86f3468d3002bcd8ca
Tran Vu Binh <tranb4010@gmail.com>
2024-04-17 20:32:21 +0700

    login with token

--- app/routers/auth.route.js
+++ app/routers/auth.route.js
@@ -8,11 +8,6 @@ const authController = require("../controllers/auth.controller");
 router.route("/login")
     .post(authController.login)
 
-<<<<<<< HEAD
-=======
-//router.route("/logout")
-//    .post(authController.logout)
->>>>>>> 3fcda5d6c577a59712b3ef4544ac5a03e1ad2610
 
 router.route("/register")
     .post(authController.register)
